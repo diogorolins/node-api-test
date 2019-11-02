991186f52188d869c2396810f2583a54a83870c8
= <=>
2019-11-02 18:37:32 -0300

    rota de autenticacao de usuarios

--- Routes/users.js
+++ Routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../Model/user')
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
   users.find({}, (err, result) => {
@@ -23,6 +24,24 @@ router.post('/', (req, res) => {
       return res.send(result);
     });
   });
+});
+
+router.post('/auth', (req, res) => {
+  const {email, password} = req.body;
+  if (!email || !password) return res.send({ error: 'Dados insufucientes' });
+
+  users.findOne({ email }, (err, result) => {
+    if (err) return res.send({ error: 'Erro ao buscar usuário' });
+    if (!result) return res.send({ error: 'Usuário não encontrado' });
+
+    bcrypt.compare(password, result.password, (err, same) => {
+      if (!same) return res.send({ error: 'Usuário não autenticado' });
+      result.password = undefined;
+      return res.send(result);
+    });
+
+  }).select('+password');
+
 });
 
 module.exports = router;
