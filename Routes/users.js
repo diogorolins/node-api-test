38f9aea7bb5cd9c7f79d1a728908a04f933522e0
= <=>
2019-11-07 23:51:02 -0300

    refatorando as rotas

--- Routes/users.js
+++ Routes/users.js
@@ -1,52 +1,13 @@
-const express = require('../Config/custom-express');
+const express = require('express');
 const router = express.Router();
-const users = require('../Model/user')
-const bcrypt = require('bcrypt');
+const UserController = require('../Controllers/UserController');
+const userController = new UserController();
 
-router.get('/', async(req, res) => {
-  try{
-      const userList = await users.find({});
-      res.send(userList);
-  } catch(err){
-      return res.send({ error: 'Erro na consulta de usuários' });
-  }
-})
+router.get('/', userController.listUsers());
 
+router.post('/', userController.createUser());
 
-router.post('/', async(req, res) => {
-  const{email, password} = req.body;
-  if (!email || !password) return res.send({ error: 'Dados insufucientes' });
-
-  try{
-    if (await users.findOne({ email })) return res.send({ error: 'Usuário já cadastrado' });
-    const user = await users.create(req.body);
-    user.password = undefined;
-    return res.send(user);
-  } 
-  catch(err){
-    console.log(err);
-    return res.send({ error: 'Erro no cadastro do usuário' });
-  }
-
-});
-
-router.post('/auth', async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) return res.send({ error: 'Dados insufucientes' });
-
-  try{
-    const user = await users.findOne({ email }).select('+password');
-    if (!await users.findOne({ email }).select('+password')) return res.send({ error: 'Usuário não encontrado' });
-    if (!await bcrypt.compare(password, user.password)) return res.send({ error: 'Usuário não autenticado' });
-    user.password = undefined;
-    return res.send(user);
-  }
-  catch(err){
-    console.log(err);
-    return res.send({ error: 'Erro na autenticação'});
-  }
-
-});
+router.post('/auth', userController.authUser() );
 
 module.exports = router;
 
