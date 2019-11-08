615213bfda4d79d2fb5ec3f6ef020a98d2f66fb8
= <=>
2019-11-07 23:52:32 -0300

    arquivos de controllers

---
+++ Controllers/UserController.js
@@ -0,0 +1,57 @@
+const users = require('../Model/user')
+const bcrypt = require('bcrypt'); 
+class UserController{
+
+  listUsers(){
+    return async (req, res) => {
+      try {
+        const userList = await users.find({});
+        res.send(userList);
+      } catch (err) {
+        return res.send({ error: 'Erro na consulta de usuários' });
+      }
+    }
+  }
+
+  createUser(){
+    return async (req, res) => {
+      const { email, password } = req.body;
+      if (!email || !password) return res.send({ error: 'Dados insufucientes' });
+
+      try {
+        if (await users.findOne({ email })) return res.send({ error: 'Usuário já cadastrado' });
+        const user = await users.create(req.body);
+        user.password = undefined;
+        return res.send(user);
+      }
+      catch (err) {
+        console.log(err);
+        return res.send({ error: 'Erro no cadastro do usuário' });
+      }
+
+    }
+  }
+
+  authUser(){
+
+    return async (req, res) => {
+      const { email, password } = req.body;
+      if (!email || !password) return res.send({ error: 'Dados insufucientes' });
+
+      try {
+        const user = await users.findOne({ email }).select('+password');
+        if (!await users.findOne({ email }).select('+password')) return res.send({ error: 'Usuário não encontrado' });
+        if (!await bcrypt.compare(password, user.password)) return res.send({ error: 'Usuário não autenticado' });
+        user.password = undefined;
+        return res.send(user);
+      }
+      catch (err) {
+        console.log(err);
+        return res.send({ error: 'Erro na autenticação' });
+      }
+
+    }
+  }
+}
+
+module.exports = UserController;
